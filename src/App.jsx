26ffe5ad14aaf9f2bb37c777884924804789ce44
Zarifa <zarifarahmani99@gmail.com>
2026-01-07 00:41:13 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Badge from "./components/Badge"
+function App(){
+  return(
+    <div>
+      <Badge label="Course" value="React" />
+      <Badge label="Course" value="React" />
+      <Badge label="Course" value="React" />
+      <Badge label="Course" value="React" />
+    </div>
+  )
+}
+export default App
---
+++ src/components/Badge.jsx
@@ -0,0 +1,8 @@
+export default function Badge({label, value}){
+    return(
+        <div>
+            <p>{label}:</p>
+            <p>{value}</p>
+        </div>
+    )
+}
